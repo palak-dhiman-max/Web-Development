deee3c59746ec7912784b23bfdf6b22912f0d77b
palak-dhiman-max <uk1211417@gmail.com>
2026-01-31 20:45:31 +0530

    file

---
+++ projects/project4/project4.js
@@ -0,0 +1,31 @@
+ const form = document.querySelector('form');
+ form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const income = document.getElementById("income");
+    const amount = parseInt(income.value);
+    let tax =0;
+    if(amount<=1200000){
+        tax = 0;
+    }
+    else if(amount<=1600000){
+        tax+= (amount-1200000)*0.15;
+    }
+
+    else if(amount<=2000000){
+        tax+=(amount-1600000)*0.20 + 60000;
+    }
+
+    else if(amount<=2400000){
+        tax += (amount-2000000)*0.25+60000+80000;
+    }
+
+    else{
+        tax +=(amount-2400000)*0.30 +60000+80000+100000;
+    }
+
+    const head = document.querySelector('h2');
+    head.textContent=`Income Tax is : ${tax}`;
+
+    form.reset();
+
+})
